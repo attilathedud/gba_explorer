7c79f86b2639fa0dd33c2849ca325b7b9863ff07
attilathedud <attilathedud@gmail.com>
2018-06-06 15:39:46 -0700

    Some text searching stuff

--- src/app/components/Strings.vue
+++ src/app/components/Strings.vue
@@ -11,23 +11,56 @@
     </div>
 </template>
 
+
 <script>
+import sww from 'simple-web-worker';
+
 export default {
     name: 'Strings',
     data: function() {
         return {
             searchText: '',
-            isSearching: false
+            isSearching: false,
+            worker : {}
         }
     },
     props: {
         rom : Buffer
     },
+    created: function() {
+        this.worker = sww.create([{ message: 'search', 
+            func: function (rom, searchText) {
+                let matches = [];
+
+                for(let i = 0; i < rom.byteLength - searchText.length; i++ ) {
+                    let match_score = 0;
+
+                    for( let c = 0; c < searchText.length - 1; c++ ) {
+                        if( rom[ i + ( c * 2 ) ] + ( searchText[ c + 1 ].charCodeAt() - searchText[ c ].charCodeAt() ) == rom[ i + ( c * 2 ) + 2 ] ) {
+                            match_score++;
+                        }
+                    }
+
+                    if( match_score >= searchText.length - 1 - 1) {
+                        matches.push(i);
+                    }
+                }
+
+                return {'matches': matches, 'rom' : rom, 'searchText' : searchText};
+            }
+        }]);
+    },
     methods : {
         startSearch: function() {
             this.isSearching = true;
 
-            
+            this.worker.postMessage('search', [this.rom, this.searchText])
+                .then(results => {
+                    results.matches.forEach(function(address) {
+                        console.log( address + " :: " + results.rom.slice(address, address+results.searchText.length));
+                    });
+                    this.isSearching = false;
+                });
         }
     }
 };
