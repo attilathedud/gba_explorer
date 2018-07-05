6092f3a1eb8d05bc0a9dd0a41539c307b0e92480
attilathedud <attilathedud@gmail.com>
2018-07-05 05:09:05 -0700

    More work on the graphics section

--- src/app/components/Graphics.vue
+++ src/app/components/Graphics.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div id="graphics-view">
         <div class="field has-addons">
             <p class="control">
                 <a class="button is-static">
@@ -14,9 +14,17 @@
             </p>
         </div>
         
-        <div style="display: flex; flex-wrap: wrap;">
-            <div v-for="pixel in bpp4_parts" v-bind:key="pixel.id" :style="{backgroundColor: getPalleteColor(pixel)}" style="height: 64px; width: 12.5%;" v-on:click="moo(pixel);">
-                    
+        <div class="graphics-flex-wrapper">
+            <div class="graphics-grid-wrapper">
+                <div v-for="tile in this.tileMap" v-bind:key="tile.id">
+                    <div v-for="row in tile" v-bind:key="row.id">
+                        <div class="graphics-flex-wrapper">
+                            <div class="pixel" v-for="pixel in row" v-bind:key="pixel.id" :style="{backgroundColor: getPalleteColor(pixel)}">
+                                
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -31,8 +39,11 @@ export default {
         return {
            bpp4_parts: [],
            pallete: [],
-           offsetText: '',
-           offset: 0
+           offsetText: '00000000',
+           offset: 0,
+           tileMap: [],
+           entries: 96,
+           linesPerRow: 16
         }
     },
     computed: {
@@ -51,27 +62,67 @@ export default {
         getPalleteColor: function(pixel) {
             return this.pallete[pixel];
         },
-        moo: function(pixel) {
-            console.log(pixel);
-        },
         startSearch: function() {
             this.offset = this.getHex(this.offsetText);
             this.generatePalleteMap();
         },
         generatePalleteMap: function() {
             this.bpp4_parts = [];
-            let section = this.rom.slice(this.offset, this.offset + 200);
+            this.tileMap = [];
+
+            let section = this.rom.slice(this.offset, this.offset + (this.entries * 32));
             let binary_stream = "";
 
             for( const b of section ) {
                 binary_stream += Number(b).toString(2).padStart(8, '0');
             }
 
+            let tileIndex = 0;
+            let tileOffset = 0;
+            this.tileMap[tileIndex] = [];
+            this.tileMap[tileIndex][tileOffset] = [];
+
             for( let i = 0, j = 0; i < binary_stream.length; i += 8, j += 2 ) {
                 this.bpp4_parts[j] = parseInt(binary_stream.substr(i + 4, 4), 2);
                 this.bpp4_parts[j+1] = parseInt(binary_stream.substr(i, 4), 2);
+
+                this.tileMap[tileIndex][tileOffset].push(this.bpp4_parts[j]);
+                this.tileMap[tileIndex][tileOffset].push(this.bpp4_parts[j+1]);
+                if((j+2) % 8 == 0) {
+                    tileOffset++;
+                    if( tileOffset != 8 ) {
+                        this.tileMap[tileIndex][tileOffset] = [];
+                    }
+                }
+                if( tileOffset == 8 ) {
+                    tileIndex++;
+                    tileOffset = 0;
+                    this.tileMap[tileIndex] = [];
+                    this.tileMap[tileIndex][tileOffset] = [];
+                }
             }
+        },
+        handleScroll: function(event) {
+            if(event.deltaY > 0) {
+                //todo: don't allow scroll past end of byte buffer (this.rom.byteLength)
+                this.offset+= this.linesPerRow * 32;
+            }
+            else {
+                if( this.offset === 0 )
+                    return;
+
+                this.offset -= this.linesPerRow * 32;
+            }
+
+            this.offsetText = this.toHexString(this.offset, 8);
+            this.generatePalleteMap();
         }
+    },
+    mounted: function() {
+        document.getElementById('graphics-view').addEventListener('wheel', this.handleScroll);
+    },
+    unmounted: function () {
+        document.getElementById('graphics-view').removeEventListener('wheel', this.handleScroll);
     }
 };
 </script>
