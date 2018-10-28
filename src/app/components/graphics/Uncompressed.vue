f919a49d11e7974ceb6a93060608d3c984b9fdd3
Nathan Tucker <abezhat@verizon.net>
2018-10-27 17:28:21 -0700

    Refactor: Changing display to canvas to speed up rendering time

--- src/app/components/graphics/Uncompressed.vue
+++ src/app/components/graphics/Uncompressed.vue
@@ -7,7 +7,8 @@
         </a>
       </p>
       <p class="control is-expanded">
-        <input 
+        <input
+          id="search-input"
           v-model="offsetText" 
           class="input" 
           type="text" 
@@ -19,27 +20,8 @@
           @click="startSearch">Search</a>
       </p>
     </div>
-        
-    <div class="graphics-flex-wrapper">
-      <div 
-        class="graphics-grid-wrapper" 
-        :style="{gridTemplateColumns: 'repeat(' + linesPerRow + ', 1fr)'}">
-        <div 
-          v-for="tile in tileMap" 
-          :key="tile.id">
-          <div 
-            v-for="row in tile" 
-            :key="row.id">
-            <div class="graphics-flex-wrapper">
-              <div 
-                v-for="pixel in row" 
-                :key="pixel.id"  
-                :style="{backgroundColor: getPalleteColor(pixel), width: pixelSizes[pixelSize] + 'px', height: pixelSizes[pixelSize] + 'px'}" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+    
+    <canvas id="canvas" />
 
     <div class="graphics-zoom">
       <span class="icon is-large">
@@ -67,9 +49,13 @@ export default {
             offsetText: "00000000",
             offset: 0,
             tileMap: {},
-            linesPerRow: 12,
+            tilesPerRow: 0,
             pixelSize: 3,
-            pixelSizes: [1, 2, 4, 8]
+            pixelSizes: [1, 2, 4, 8],
+            sectionLength: 128,
+            tileSize: 8,
+            canvas: undefined,
+            ctx: undefined
         };
     },
     computed: {
@@ -81,12 +67,16 @@ export default {
         for( let i = 0; i < 16; i++ ) {
             this.pallete[i] = "rgb(" + i * 16 + "," + i * 16 + "," + i * 16 + ")";
         }
-
-        this.generatePalleteMap();
     },
     mounted: function() {
         window.addEventListener("wheel", this.handleScroll);
         window.addEventListener("keydown", this.handleKeypress);
+
+        this.canvas = document.getElementById("canvas");
+        this.ctx = this.canvas.getContext("2d"); 
+        
+        this.tilesPerRow = Math.floor(this.canvas.width / (this.pixelSizes[this.pixelSize] * this.tileSize)); 
+        this.generatePalleteMap();
     },
     methods: {
         getPalleteColor: function(pixel) {
@@ -97,7 +87,7 @@ export default {
             this.generatePalleteMap();
         },
         generatePalleteMap: function() {
-            let section = this.rom.slice(this.offset, this.offset + (this.linesPerRow * 8 * 32));
+            let section = this.rom.slice(this.offset, this.offset + (this.tilesPerRow * this.tileSize * this.sectionLength));
             
             this.tileMap = {};
             
@@ -111,34 +101,68 @@ export default {
                 this.tileMap[blockIndex][rowIndex].push(section[i] & 0b00001111);
                 this.tileMap[blockIndex][rowIndex].push((section[i] & 0b11110000) >> 4);
 
-                if(((i*2)+2) % 8 == 0) {
+                if(((i*2)+2) % this.tileSize == 0) {
                     rowIndex++;
-                    if( rowIndex != 8 ) {
+                    if( rowIndex != this.tileSize ) {
                         this.tileMap[blockIndex][rowIndex] = [];
                     }
                 }
-                if( rowIndex == 8 ) {
+                if( rowIndex == this.tileSize ) {
                     blockIndex++;
                     rowIndex = 0;
                     this.tileMap[blockIndex] = [];
                     this.tileMap[blockIndex][rowIndex] = [];
                 }
-            } 
+            }
+
+            let pixelIndex = 0;
+            let tileIndex = 0;
+            rowIndex = 0;
+            let tileRowIndex = 0;
+
+            if( this.ctx != undefined ) {
+                let pixelSize = this.pixelSizes[this.pixelSize];
+
+                this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+                
+                for( const tile in this.tileMap ) {
+                    for( const row in this.tileMap[tile] ) {
+                        for( const pixel in this.tileMap[tile][row] ) {
+                            let pixelData = this.tileMap[tile][row][pixel];
+
+                            this.ctx.fillStyle = this.getPalleteColor(pixelData); 
+                            this.ctx.fillRect(
+                                pixelIndex * pixelSize + (tileIndex * pixelSize * this.tileSize), 
+                                rowIndex * pixelSize + (tileRowIndex * pixelSize * this.tileSize), 
+                                pixelSize, pixelSize);
+                            pixelIndex++;
+                        }
+                        pixelIndex = 0;
+                        rowIndex++;
+                    }
+                    rowIndex = 0;
+                    tileIndex++;
+                    if( tileIndex >= this.tilesPerRow ) {
+                        tileRowIndex++;
+                        tileIndex = 0;
+                    }
+                } 
+            }
         },
         scrollDown: function() {
-            if( this.offset + this.linesPerRow * 32 > this.rom.byteLength )
+            if( this.offset + this.tilesPerRow * this.sectionLength > this.rom.byteLength )
                 return;
 
-            this.offset += this.linesPerRow * 32;
+            this.offset += this.tilesPerRow * this.sectionLength;
 
             this.offsetText = this.toHexString(this.offset, 8);
-            this.generatePalleteMap();
+            this.generatePalleteMap(); 
         },
         scrollUp: function() {
             if( this.offset === 0 )
                 return;
 
-            this.offset -= this.linesPerRow * 32;
+            this.offset -= this.tilesPerRow * this.sectionLength;
 
             this.offsetText = this.toHexString(this.offset, 8);
             this.generatePalleteMap(); 
@@ -157,7 +181,7 @@ export default {
             const KEY_RIGHT = 39;
             const KEY_DOWN = 40;
 
-            if( document.getElementById("search-input") == document.activeElement )
+            if( document.getElementById("search-input") == document.activeElement && (event.which == KEY_RIGHT || event.which == KEY_LEFT))
                 return;
 
             switch( event.which ) {
@@ -174,14 +198,18 @@ export default {
         zoomIn: function() { 
             if( this.pixelSize < 3 ) {
                 this.pixelSize++;
-                this.linesPerRow /= 2;
+
+                this.tilesPerRow = Math.floor(this.canvas.width / (this.pixelSizes[this.pixelSize] * this.tileSize)); 
+ 
                 this.generatePalleteMap();
             }
         },
         zoomOut: function() {
             if( this.pixelSize > 0 ) {
                 this.pixelSize--;
-                this.linesPerRow *= 2;
+               
+                this.tilesPerRow = Math.floor(this.canvas.width / (this.pixelSizes[this.pixelSize] * this.tileSize));  
+                
                 this.generatePalleteMap();
             }
         }
